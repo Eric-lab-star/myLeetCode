e37efacf7df406c43f3df7daf530d2e3b38b7038
eric-lab-star <cyon256@icloud.com>
2023-02-25 15:49:24 +0900

    end

--- p9/main.go
+++ p9/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	input := 0
+	input := -131
 
 	ans := isPalindrome(input)
 
@@ -11,33 +11,14 @@ func main() {
 
 }
 func isPalindrome(x int) bool {
-	if x < 0 {
-		return false
-	}
-	if x == 0 {
-		return true
-	}
-	a := IntToSlice(x, []int{})
-	s := 0
-	e := len(a) - 1
+	x_copy := x
+	reverseX := 0
 
-	for s <= (len(a)-1)/2 {
-		if a[s] == a[e] {
-			s++
-			e--
-			continue
-		}
-		return false
+	for x_copy > 0 {
+		reverseX = reverseX*10 + x_copy%10
+		x_copy /= 10
 	}
 
-	return true
-}
+	return reverseX == x
 
-func IntToSlice(n int, sequence []int) []int {
-	if n != 0 {
-		i := n % 10
-		sequence = append([]int{i}, sequence...)
-		return IntToSlice(n/10, sequence)
-	}
-	return sequence
 }
