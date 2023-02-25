76d03663dd516d7a9f372d03cad204b014ff48a0
eric-lab-star <cyon256@icloud.com>
2023-02-25 12:20:09 +0900

    failed 1

---
+++ p121/go.mod
@@ -0,0 +1,3 @@
+module github.com/Eric-lab-star/LeetCode/p121
+
+go 1.20
---
+++ p121/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	ret := maxProfit(input)
+	fmt.Println(ret)
+
+}
+
+var input = []int{7, 1, 5, 3, 6, 4}
+
+func maxProfit(prices []int) int {
+	var max int
+	for i := 0; i < len(prices); i++ {
+		for j := i + 1; j < len(prices); j++ {
+			if prices[j] > prices[i] {
+				test := prices[j] - prices[i]
+				if test > max {
+					max = test
+				}
+			}
+			continue
+
+		}
+	}
+	return max
+}
