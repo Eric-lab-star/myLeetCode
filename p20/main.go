6d543612a20d73da8e0bb60c6809ca5141216814
eric-lab-star <cyon256@icloud.com>
2023-03-13 20:36:44 +0900

    p20

--- p20/main.go
+++ p20/main.go
@@ -3,5 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello")
+	ret := isValid("([)")
+	fmt.Println(ret)
+
+}
+
+func isValid(s string) bool {
+	var parenthese = map[rune]rune{
+		'(': ')',
+		'{': '}',
+		'[': ']',
+	}
+	var keep = []rune{}
+	if len(s)%2 == 1 {
+		return false
+	}
+	for i := 0; i < len(s); i++ {
+		fmt.Println(i)
+		fmt.Println(string(keep))
+		v, ok := parenthese[rune(s[i])]
+
+		if ok {
+			keep = append(keep, v)
+			continue
+		}
+		if !ok && keep[len(keep)-1] == rune(s[i]) {
+			keep = keep[:len(keep)-1]
+			continue
+		}
+		if !ok && keep[len(keep)-1] != rune(s[i]) {
+			return false
+
+		}
+	}
+	return true
 }
