305c4f77b2588c0c211bc69b0f3eab7e3ed0dfdf
eric-lab-star <cyon256@icloud.com>
2023-03-15 15:21:25 +0900

    p26

--- p21/main.go
+++ p21/main.go
@@ -4,11 +4,29 @@ import (
 	"fmt"
 )
 
-func main() {
-	fmt.Println("heelo")
+var l1 = ListNode{1, &l1_1}
+var l1_1 = ListNode{3, &l1_2}
+var l1_2 = ListNode{5, nil}
 
-	ret := []int{}
-	fmt.Println(ret)
+var l2 = ListNode{2, &l2_1}
+var l2_1 = ListNode{4, &l2_2}
+var l2_2 = ListNode{6, nil}
+
+func main() {
+	merge := ListNode{}
+	l1v := values(l1)
+	l2v := values(l2)
+	fmt.Printf("l1v : %v\n", l1v)
+	fmt.Printf("l2v : %v\n", l2v)
+	ret := mergeTwoLists(&l1, &l2, &merge)
+	retv := values(*ret)
+	l1v = values(l1)
+	l2v = values(l2)
+	fmt.Printf("retv : %v\n", retv)
+
+	fmt.Printf("l1v : %v\n", l1v)
+	fmt.Printf("l2v : %v\n", l2v)
+
 }
 
 /**
@@ -23,24 +41,34 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
-	result := &ListNode{}
-	current := result
-
-	for list1 != nil && list2 != nil {
-		if list1.Val < list2.Val {
-			current.Next = list1
-			list1 = list1.Next
-		} else {
-			current.Next = list2
-			list2 = list2.Next
+func values(list ListNode) []int {
+	ret := []int{}
+	for {
+		ret = append(ret, list.Val)
+		if list.Next == nil {
+			break
 		}
+		list = *list.Next
 	}
-	if list1 != nil {
-		current.Next = list2
-	} else {
-		current.Next = list1
-	}
-	return result.Next
+	return ret
+}
 
+func mergeTwoLists(l1 *ListNode, l2 *ListNode, merge *ListNode) *ListNode {
+
+	if l1 == nil {
+		merge.Next = l2
+		return merge.Next
+	}
+	if l2 == nil {
+		merge.Next = l1
+		return merge.Next
+	}
+	if l1.Val < l2.Val {
+		merge.Next = l1
+		mergeTwoLists(l1.Next, l2, merge.Next) //1
+		return merge.Next                      //2
+	}
+	merge.Next = l2
+	mergeTwoLists(l1, l2.Next, merge.Next)
+	return merge.Next
 }
---
+++ p26/go.mod
@@ -0,0 +1,3 @@
+module github.com/Eric-lab-star/LeetCode/p26
+
+go 1.20
