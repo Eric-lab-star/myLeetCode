4dc5397325be068037ee4b529188b0508cea6c2b
eric-lab-star <cyon256@icloud.com>
2023-05-21 11:12:10 +0900

    p69

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	input := 2147395599
+	output := mySqrt(input)
+	fmt.Println(output)
 }
 
 func mySqrt(x int) int {
-
+	z := float64(1)
+	floatX := float64(x)
+	z = z - (z*z-floatX)/(2*z)
+	for {
+		newZ := z - (z*z-floatX)/(2*z)
+		if z-newZ < 0.00001 {
+			return int(z)
+		}
+		z = newZ
+	}
 }
