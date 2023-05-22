59c1f0b4fddbb39ccdb621fa6ffb6b24faea4c42
eric-lab-star <cyon256@icloud.com>
2023-05-22 11:57:25 +0900

    wip' ; '

--- main.go
+++ main.go
@@ -1,24 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	input := 4
-	output := climbStairs(input)
+	a := "111"
+	b := "1"
+	output := addBinary(a, b)
 	fmt.Println(output)
+}
+
+func addBinary(a string, b string) string {
+	sA := toSlice(a)
+	sB := toSlice(b)
+	extraOne := false
+
+	for i := len(sA) - 1; i >= 0; i-- {
+		if extraOne && sA[i] == 49 {
+			sA[i] = 48
+		}
+		if sA[i] == 49 && sB[0] == 49 {
+			sA[i] = 48
+			extraOne = true
+		} else if sA[i] == 49 && sB[0] == 48 || sA[i] == 48 && sB[0] == 49 {
+			sA[i] = 49
+		} else {
+			sA[i] = 48
+		}
+
+	}
+	return string(sA)
 
 }
 
-func climbStairs(n int) int {
-	if n <= 2 {
-		return n
+func toSlice(i string) (slice []byte) {
+	for _, v := range i {
+		slice = append(slice, byte(v))
 	}
+	return
 
-	a, b := 1, 2
-
-	for i := 2; i < n; i++ {
-		a, b = b, a+b
-	}
-
-	return b
 }
+
+/*
+
+
+ */
