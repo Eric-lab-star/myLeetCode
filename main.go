0bbdd22f19f6c12138e173f7d02d88f444737906
eric-lab-star <cyon256@icloud.com>
2023-05-22 09:54:15 +0900

    climbing stairs

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	input := 4
+	output := climbStairs(input)
+	fmt.Println(output)
+
+}
+
+func climbStairs(n int) int {
+	if n <= 2 {
+		return n
+	}
+
+	a, b := 1, 2
+
+	for i := 2; i < n; i++ {
+		a, b = b, a+b
+	}
+
+	return b
 }
