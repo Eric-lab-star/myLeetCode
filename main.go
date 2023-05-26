5086b9c972ad83b87e2932e4ef1e8e6aa5076497
eric-lab-star <cyon256@icloud.com>
2023-05-26 10:06:00 +0900

    9.Palindrome Number

--- main.go
+++ main.go
@@ -1,22 +1,8 @@
 package main
 
-import (
-	"fmt"
-)
-
+/*
+palindrome number
+*/
 func main() {
-	input := []int{11, 14, 2, 7}
-	output := twoSum(input, 9)
-	fmt.Println(output)
-}
 
-func twoSum(nums []int, target int) []int {
-	finder := make(map[int]int)
-	for i, v := range nums {
-		if j, ok := finder[target-v]; ok {
-			return []int{j, i}
-		}
-		finder[v] = i
-	}
-	return []int{}
 }
