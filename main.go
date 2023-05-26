0379558973475005a9458a51fe15f3d59a3208c1
eric-lab-star <cyon256@icloud.com>
2023-05-26 09:02:00 +0900

    twosum: better soluction

--- main.go
+++ main.go
@@ -11,14 +11,12 @@ func main() {
 }
 
 func twoSum(nums []int, target int) []int {
-	var output []int
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				output := []int{i, j}
-				return output
-			}
+	finder := make(map[int]int)
+	for i, v := range nums {
+		if j, ok := finder[target-v]; ok {
+			return []int{j, i}
 		}
+		finder[v] = i
 	}
-	return output
+	return []int{}
 }
